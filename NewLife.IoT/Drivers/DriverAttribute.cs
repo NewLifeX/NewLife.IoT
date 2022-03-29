e997436dea3fedd99aa5a1d887d3d855e1a16efa
大石头 <nnhy@vip.qq.com>
2022-03-29 13:42:48 +0800

    代码整理

--- NewLife.IoT/Drivers/DriverAttribute.cs
+++ NewLife.IoT/Drivers/DriverAttribute.cs
@@ -1,17 +1,16 @@
-﻿namespace NewLife.IoT.Drivers
-{
-    /// <summary>驱动特性</summary>
-    public class DriverAttribute : Attribute
-    {
-        /// <summary>
-        /// 名称
-        /// </summary>
-        public String Name { get; set; }
+﻿namespace NewLife.IoT.Drivers;
 
-        /// <summary>
-        /// 指定驱动名称
-        /// </summary>
-        /// <param name="name"></param>
-        public DriverAttribute(String name) => Name = name;
-    }
+/// <summary>驱动特性</summary>
+public class DriverAttribute : Attribute
+{
+    /// <summary>
+    /// 名称
+    /// </summary>
+    public String Name { get; set; }
+
+    /// <summary>
+    /// 指定驱动名称
+    /// </summary>
+    /// <param name="name"></param>
+    public DriverAttribute(String name) => Name = name;
 }
--- NewLife.IoT/ThingModels/IPoint.cs
+++ NewLife.IoT/ThingModels/IPoint.cs
@@ -1,18 +1,17 @@
-﻿namespace NewLife.IoT.ThingModels
+﻿namespace NewLife.IoT.ThingModels;
+
+/// <summary>点位</summary>
+public interface IPoint
 {
-    /// <summary>点位</summary>
-    public interface IPoint
-    {
-        /// <summary>名称</summary>
-        String Name { get; set; }
+    /// <summary>名称</summary>
+    String Name { get; set; }
 
-        /// <summary>地址。常规地址 0x06，大地址 40000，位域地址 0x46000:05，比特位置0~15</summary>
-        String Address { get; set; }
+    /// <summary>地址。常规地址 0x06，大地址 40000，位域地址 0x46000:05，比特位置0~15</summary>
+    String Address { get; set; }
 
-        /// <summary>数据类型。来自物模型</summary>
-        String Type { get; set; }
+    /// <summary>数据类型。来自物模型</summary>
+    String Type { get; set; }
 
-        /// <summary>大小。数据字节数，或字符串长度，Modbus寄存器一般占2个字节</summary>
-        Int32 Length { get; set; }
-    }
+    /// <summary>大小。数据字节数，或字符串长度，Modbus寄存器一般占2个字节</summary>
+    Int32 Length { get; set; }
 }
