071fdc771d936164008010232e521432e5bbed5f
大石头 <nnhy@vip.qq.com>
2022-05-10 17:50:35 +0800

    在某些场景中，特殊点位地址（如#）表示虚拟地址，该点位数值由ReadRule表达式动态计算得到，点位信息并不会传递给驱动层

--- NewLife.IoT/ThingModels/PointModel.cs
+++ NewLife.IoT/ThingModels/PointModel.cs
@@ -7,6 +7,7 @@ public class PointModel : IPoint
     public String Name { get; set; }
 
     /// <summary>地址。表示点位的地址，具体含义由设备驱动决定。例如常规地址6，字母地址DI07，Modbus地址4x0015，位域地址D012.05，比特位置0~15</summary>
+    /// <remarks>在某些场景中，特殊点位地址（如#）表示虚拟地址，该点位数值由ReadRule表达式动态计算得到，点位信息并不会传递给驱动层</remarks>
     public String Address { get; set; }
 
     /// <summary>数据类型。来自物模型</summary>
