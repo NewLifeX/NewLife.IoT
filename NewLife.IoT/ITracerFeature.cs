a6ae65ade5ec44eb028530f5f511af342f879f1a
大石头 <nnhy@vip.qq.com>
2022-05-13 16:56:08 +0800

    给NET45加上ILogFeature

---
+++ NewLife.IoT/ITracerFeature.cs
@@ -0,0 +1,10 @@
+﻿#if !NETSTANDARD
+namespace NewLife.Log;
+
+/// <summary>日志功能接口</summary>
+public interface ITracerFeature
+{
+    /// <summary>性能追踪</summary>
+    ITracer Tracer { get; set; }
+}
+#endif
