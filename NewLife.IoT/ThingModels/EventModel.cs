3e74a58b1b9639c2b0ff12c3b766291edef0b932
xiyunfei <xi3892@163.com>
2023-09-23 19:29:39 +0800

    取消时间状态字段

--- NewLife.IoT/ThingModels/EventModel.cs
+++ NewLife.IoT/ThingModels/EventModel.cs
@@ -14,7 +14,4 @@ public class EventModel
 
     /// <summary>内容。事件详情</summary>
     public String Remark { get; set; }
-
-    /// <summary>是否启用</summary>
-    public Boolean IsEnable { get; set; }
 }
