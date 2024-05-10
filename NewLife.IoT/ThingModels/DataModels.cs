41257dd22f6048e4acd8d6d5b36bdbd2a7ce8157
猿人易 <qcjxberin@outlook.com>
2024-05-10 17:48:34 +0800

    数据集合增加消息编号字段用于上下行消息对应以及日志排查

--- NewLife.IoT/ThingModels/DataModels.cs
+++ NewLife.IoT/ThingModels/DataModels.cs
@@ -3,6 +3,9 @@
 /// <summary>数据集合</summary>
 public class DataModels
 {
+    /// <summary>消息编号</summary>
+    public Int64 Id { get; set; }
+
     /// <summary>设备编码</summary>
     public String DeviceCode { get; set; } = null!;
 
