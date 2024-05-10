9fca42a93666e34701ea9015302760827de9bab8
猿人易 <qcjxberin@outlook.com>
2024-05-10 19:18:08 +0800

    增加数据集合消息编号注释

--- NewLife.IoT/ThingModels/DataModels.cs
+++ NewLife.IoT/ThingModels/DataModels.cs
@@ -3,7 +3,7 @@
 /// <summary>数据集合</summary>
 public class DataModels
 {
-    /// <summary>消息编号</summary>
+    /// <summary>消息编号。适用于多人同时操作，同一任务需要服务器端多指令串行下发等场景</summary>
     public Int64 Id { get; set; }
 
     /// <summary>设备编码</summary>
