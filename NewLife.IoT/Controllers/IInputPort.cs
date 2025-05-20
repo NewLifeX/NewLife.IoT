3a26906da1a7b96672c3905d1b797aa1eab268bf
智能大石头 <nnhy@vip.qq.com>
2025-05-21 01:01:42 +0800

    [fix] 修正读取文件输入口的错误，不能使用FileAllText，因为文件不仅仅一个字符

--- NewLife.IoT/Controllers/IInputPort.cs
+++ NewLife.IoT/Controllers/IInputPort.cs
@@ -9,16 +9,32 @@ public interface IInputPort
 }
 
 /// <summary>文件驱动输入口</summary>
-public class FileInputPort : IInputPort
+/// <remarks>实例化文件驱动输入口</remarks>
+/// <param name="fileName"></param>
+public class FileInputPort(String fileName) : DisposeBase, IInputPort
 {
     /// <summary>文件路径</summary>
-    public String FileName { get; set; } = null!;
+    public String FileName { get; set; } = fileName;
 
-    /// <summary>实例化文件驱动输入口</summary>
-    /// <param name="fileName"></param>
-    public FileInputPort(String fileName) => FileName = fileName;
+    private FileStream? _fs;
+
+    /// <summary>销毁</summary>
+    /// <param name="disposing"></param>
+    protected override void Dispose(Boolean disposing)
+    {
+        base.Dispose(disposing);
+
+        _fs.TryDispose();
+    }
+
+    private FileStream GetFile() => _fs ??= new FileStream(FileName.GetFullPath(), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
 
     /// <summary>读取开关值</summary>
     /// <returns></returns>
-    public virtual Boolean Read() => File.ReadAllText(FileName.GetFullPath())?.Trim() == "1";
+    public virtual Boolean Read()
+    {
+        var fs = GetFile();
+
+        return fs.ReadByte() == '1';
+    }
 }
--- NewLife.IoT/Controllers/IOutputPort.cs
+++ NewLife.IoT/Controllers/IOutputPort.cs
@@ -13,20 +13,41 @@ public interface IOutputPort
 }
 
 /// <summary>文件驱动输出口</summary>
-public class FileOutputPort : IOutputPort
+/// <remarks>实例化文件驱动输出口</remarks>
+/// <param name="fileName"></param>
+public class FileOutputPort(String fileName) : DisposeBase, IOutputPort
 {
     /// <summary>文件路径</summary>
-    public String FileName { get; set; }
+    public String FileName { get; set; } = fileName;
 
-    /// <summary>实例化文件驱动输出口</summary>
-    /// <param name="fileName"></param>
-    public FileOutputPort(String fileName) => FileName = fileName;
+    private FileStream? _fs;
+
+    /// <summary>销毁</summary>
+    /// <param name="disposing"></param>
+    protected override void Dispose(Boolean disposing)
+    {
+        base.Dispose(disposing);
+
+        _fs.TryDispose();
+    }
+
+    private FileStream GetFile() => _fs ??= new FileStream(FileName.GetFullPath(), FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
 
     /// <summary>读取开关值</summary>
     /// <returns></returns>
-    public virtual Boolean Read() => File.ReadAllText(FileName.GetFullPath())?.Trim() == "1";
+    public virtual Boolean Read()
+    {
+        var fs = GetFile();
+
+        return fs.ReadByte() == '1';
+    }
 
     /// <summary>写入开关值</summary>
     /// <param name="value"></param>
-    public virtual void Write(Boolean value) => File.WriteAllText(FileName.GetFullPath(), value ? "1" : "0");
+    public virtual void Write(Boolean value)
+    {
+        var fs = GetFile();
+        fs.WriteByte((Byte)(value ? '1' : '0'));
+        fs.Flush();
+    }
 }
